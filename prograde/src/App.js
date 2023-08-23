01b13921b899929d564930da0306affd9374da03
laura-williams-1 <laurawilliams@pursuit.org>
2023-08-23 19:30:08 -0400

    added react-router- dom to implement routes and cleaned up app.js

--- prograde/src/App.js
+++ prograde/src/App.js
@@ -1,22 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import ServiceAlerts from "./Components/ServiceAlerts";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Hello</div>
+      <Routes>
+        <Route path="service-alerts" element={<ServiceAlerts />} />
+      </Routes>
     </div>
   );
 }
