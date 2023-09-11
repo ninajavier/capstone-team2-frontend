7c390e7aec2364f0085618e8150731c6a3914b18
laura-williams-1 <laurawilliams@pursuit.org>
2023-09-11 09:21:22 -0400

    imported all icons into an object in a new  index.js file

---
+++ prograde/src/Assets/TrainIcons/index.js
@@ -0,0 +1,53 @@
+import _7_digit from "./7-digit.svg";
+import sir from "./sir.svg";
+import z_letter from "./z-letter.svg";
+import j_letter from "./j-letter.svg";
+import w_letter from "./w-letter.svg";
+import s_letter from "./s-letter.svg";
+import g_letter from "./g-letter.svg";
+import l_letter from "./l-letter.svg";
+import q_letter from "./q-letter.svg";
+import m_letter from "./m-letter.svg";
+import e_letter from "./e-letter.svg";
+import b_letter from "./b-letter.svg";
+import f_letter from "./f-letter.svg";
+import d_letter from "./d-letter.svg";
+import r_letter from "./r-letter.svg";
+import _6_digit from "./6-digit.svg";
+import n_letter from "./n-letter.svg";
+import c_letter from "./c-letter.svg";
+import _5_digit from "./5-digit.svg";
+import a_letter from "./a-letter.svg";
+import _4_digit from "./4-digit.svg";
+import _3_digit from "./3-digit.svg";
+import _2_digit from "./2-digit.svg";
+import _1_digit from "./1-digit.svg";
+
+const icons = {
+  _7_digit,
+  sir,
+  z_letter,
+  j_letter,
+  w_letter,
+  s_letter,
+  g_letter,
+  l_letter,
+  q_letter,
+  m_letter,
+  e_letter,
+  b_letter,
+  f_letter,
+  d_letter,
+  r_letter,
+  _6_digit,
+  n_letter,
+  c_letter,
+  _5_digit,
+  a_letter,
+  _4_digit,
+  _3_digit,
+  _2_digit,
+  _1_digit,
+};
+
+export default icons;
