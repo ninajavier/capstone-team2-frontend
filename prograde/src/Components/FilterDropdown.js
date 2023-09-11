803f5316c258ec714c6c771f454ae98d244b3fac
laura-williams-1 <laurawilliams@pursuit.org>
2023-09-11 01:47:43 -0400

    inital from set up for dropdown

--- prograde/src/Components/FilterDropdown.js
+++ prograde/src/Components/FilterDropdown.js
@@ -1,7 +1,32 @@
-import { Dropdown } from "react-bootstrap";
-
 const FilterDropdown = () => {
-  return <Dropdown></Dropdown>;
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <select multiple>
+        <option value="a">A</option>
+        <option value="b">B</option>
+        <option value="c">C</option>
+        <option value="d">D</option>
+        <option value="e">E</option>
+        <option value="f">F</option>
+        <option value="g">G</option>
+        <option value="j">J</option>
+        <option value="l">L</option>
+        <option value="m">M</option>
+        <option value="n">N</option>
+        <option value="q">Q</option>
+        <option value="r">R</option>
+        <option value="w">W</option>
+        <option value="lirr">LIRR</option>
+        <option value="mnr">MNR</option>
+        <option value="sirr">SIR</option>
+
+        <option value="c">L</option>
+      </select>
+    </form>
+  );
 };
 
 export default FilterDropdown;
