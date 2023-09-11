5681441563352e0c875e715cc798f44bc1feaffb
laura-williams-1 <laurawilliams@pursuit.org>
2023-09-10 22:32:08 -0400

    initalize filter dropdown component

--- prograde/src/App.js
+++ prograde/src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import SubwayAlerts from "./Components/SubwayAlerts";
+import FilterDropdown from "./Components/FilterDropdown";
 function App() {
   return (
     <div>
       <div>Hello</div>
       <Routes>
         <Route path="subway-alerts" element={<SubwayAlerts />} />
+        <Route path="dropdown" element={<FilterDropdown />} />
       </Routes>
     </div>
   );
