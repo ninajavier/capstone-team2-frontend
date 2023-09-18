d5a4f42d69111b70e6b5229c255703ad98bec8ca
laura-williams-1 <laurawilliams@pursuit.org>
2023-09-18 11:37:20 -0400

    fixed repated id from API call, updating styling subway alerts

--- src/Components/SubwayAlerts.js
+++ src/Components/SubwayAlerts.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Card } from "react-bootstrap";
 import axios from "axios";
 export default function SubwayAlerts() {
   const [subwayAlerts, setSubwayAlerts] = useState({});
@@ -17,7 +18,9 @@ export default function SubwayAlerts() {
     const date = new Date(timeStamp * 1000);
     return date.toLocaleString("en-US", options);
   };
-
+  const generateUniqueKey = (id, index) => {
+    return `${id}_${index}`;
+  };
   useEffect(() => {
     const apiUrl = "http://localhost:8888/subway-alerts";
 
@@ -34,32 +37,38 @@ export default function SubwayAlerts() {
   console.log("subway-alerts", subwayAlerts);
   return (
     <div>
-      <h1>Subway Alerts</h1>
-      {subwayAlerts.entity ? (
-        <ul>
-          {subwayAlerts.entity.map((entity) => (
-            <li key={entity.id}>
-              <strong>PROGRADE LIVE ALERT</strong>{" "}
-              {entity.alert.headerText.translation[0].text}
-              <br />
-              <strong>Goes Into Affect on</strong>{" "}
-              {ESTHandler(entity.alert.activePeriod[0].start)}
-              <br />
-              <strong>Current Train Lines Affected</strong>
-              <ul>
-                {entity.alert.informedEntity.map((informedEntity, index) => (
-                  <li key={index}>
-                    {informedEntity.routeId}
-                    {/* {informedEntity.stopId || "N/A"} */}
-                  </li>
-                ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <Card className="card">
+        <Card.Body>
+          <h1>Subway Alerts</h1>
+          {subwayAlerts.entity ? (
+            <ul>
+              {subwayAlerts.entity.map((entity, index) => (
+                <li key={generateUniqueKey(entity.id, index)}>
+                  <strong>PROGRADE LIVE ALERT</strong>{" "}
+                  {entity.alert.headerText.translation[0].text}
+                  <br />
+                  <strong>Goes Into Affect on</strong>{" "}
+                  {ESTHandler(entity.alert.activePeriod[0].start)}
+                  <br />
+                  <strong>Current Train Lines Affected</strong>
+                  <ul>
+                    {entity.alert.informedEntity.map(
+                      (informedEntity, index) => (
+                        <li key={index}>
+                          {informedEntity.routeId}
+                          {/* {informedEntity.stopId || "N/A"} */}
+                        </li>
+                      )
+                    )}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </Card.Body>
+      </Card>
     </div>
   );
 }
