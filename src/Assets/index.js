7958064cd561c2aaef0b547e80eab5b8519db9fb
laura-williams-1 <laurawilliams@pursuit.org>
2023-09-18 15:11:26 -0400

    additional features to dropdown

---
+++ src/Assets/index.js
@@ -0,0 +1,51 @@
+import z_letter from "./z-letter.256x256.png";
+import j_letter from "./j-letter.256x256.png";
+import w_letter from "./w-letter.256x256.png";
+import s_letter from "./s-letter.256x256.png";
+import g_letter from "./g-letter.256x256.png";
+import l_letter from "./l-letter.256x256.png";
+import q_letter from "./q-letter.256x256.png";
+import m_letter from "./m-letter.256x256.png";
+import e_letter from "./e-letter.256x256.png";
+import _7_digit from "./7-digit.256x256.png";
+import b_letter from "./b-letter.256x256.png";
+import f_letter from "./f-letter.256x256.png";
+import d_letter from "./d-letter.256x256.png";
+import r_letter from "./r-letter.256x256.png";
+import _6_digit from "./6-digit.256x256.png";
+import n_letter from "./n-letter.256x256.png";
+import c_letter from "./c-letter.256x256.png";
+import _5_digit from "./5-digit.256x256.png";
+import a_letter from "./a-letter.256x256.png";
+import _4_digit from "./4-digit.256x256.png";
+import _3_digit from "./3-digit.256x256.png";
+import _2_digit from "./2-digit.256x256.png";
+import _1_digit from "./1-digit.256x256.png";
+
+const icons = {
+  z_letter,
+  j_letter,
+  w_letter,
+  s_letter,
+  g_letter,
+  l_letter,
+  q_letter,
+  m_letter,
+  e_letter,
+  _7_digit,
+  b_letter,
+  f_letter,
+  d_letter,
+  r_letter,
+  _6_digit,
+  n_letter,
+  c_letter,
+  _5_digit,
+  a_letter,
+  _4_digit,
+  _3_digit,
+  _2_digit,
+  _1_digit,
+};
+
+export default icons;
--- src/Components/FilterDropdown.js
+++ src/Components/FilterDropdown.js
@@ -1,5 +1,8 @@
 import { Form, Dropdown } from "react-bootstrap";
+import "./styles.css";
 import { useState } from "react";
+import icons from "../Assets/index.js";
+
 const FilterDropdown = () => {
   const submitHandler = (event) => {
     event.preventDefault();
@@ -7,6 +10,10 @@ const FilterDropdown = () => {
   const checkboxHandler = (event) => {
     event.stopPropagation();
   };
+  function isNumber(value) {
+    return Number.isFinite(Number(value));
+  }
+
   const labels = [
     "A",
     "B",
@@ -21,28 +28,52 @@ const FilterDropdown = () => {
     "N",
     "Q",
     "R",
+    "S",
     "W",
-    "LIRR",
-    "MNR",
-    "SIR",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
   ];
+  // REMVOING  "LIRR", "MNR","SIR",
+  function getIconKeyFromLabel(label) {
+    if (isNumber(label)) {
+      return `_${label}_digit`;
+    } else {
+      return `${label.toLowerCase()}_letter`;
+    }
+  }
+
+  function getIconForLabel(label) {
+    const key = getIconKeyFromLabel(label);
+    return icons[key];
+  }
 
   return (
     <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Trains
-      </Dropdown.Toggle>
-      <Dropdown.Menu>
+      <Dropdown.Toggle id="dropdown-basic">Trains</Dropdown.Toggle>
+      <Dropdown.Menu className="multi-column-dropdown">
         <Form onSubmit={submitHandler}>
           <Form.Group className="trains-dropdown">
             {labels.map((trainLabel, index) => {
+              const icon = getIconForLabel(trainLabel);
               return (
                 <Dropdown.Item onClick={checkboxHandler} key={index}>
-                  <Form.Check
-                    type="checkbox"
-                    onClick={checkboxHandler}
-                    label={trainLabel}
-                  />
+                  <div className="icon-checkbox-wrapper">
+                    <Form.Check type="checkbox" onClick={checkboxHandler} />
+                    <img
+                      src={icon}
+                      alt={`${trainLabel} icon`}
+                      style={{
+                        width: "24px",
+                        height: "24px",
+                        marginLeft: "8px",
+                      }}
+                    />
+                  </div>
                 </Dropdown.Item>
               );
             })}
