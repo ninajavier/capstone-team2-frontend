e42f565549ca5955c89b5f987e7b6863e2287bfa
laura-williams-1 <laurawilliams@pursuit.org>
2023-09-18 12:16:55 -0400

    Trains displayed in FilterDropdown component

--- src/Components/FilterDropdown.js
+++ src/Components/FilterDropdown.js
@@ -1,7 +1,56 @@
-import { Dropdown } from "react-bootstrap";
-
+import { Form, Dropdown } from "react-bootstrap";
+import { useState } from "react";
 const FilterDropdown = () => {
-  return <Dropdown></Dropdown>;
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+  const checkboxHandler = (event) => {
+    event.stopPropagation();
+  };
+  const labels = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "J",
+    "L",
+    "M",
+    "N",
+    "Q",
+    "R",
+    "W",
+    "LIRR",
+    "MNR",
+    "SIR",
+  ];
+
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Trains
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="trains-dropdown">
+            {labels.map((trainLabel, index) => {
+              return (
+                <Dropdown.Item onClick={checkboxHandler} key={index}>
+                  <Form.Check
+                    type="checkbox"
+                    onClick={checkboxHandler}
+                    label={trainLabel}
+                  />
+                </Dropdown.Item>
+              );
+            })}
+          </Form.Group>
+        </Form>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
 };
 
 export default FilterDropdown;
