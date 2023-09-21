89807cd4379d20ab77d5eed4a98f53fcff403fbf
ninajavier <ninajavier13@gmail.com>
2023-09-21 14:51:46 -0400

    Fixed axios call for generating comments

--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+const API = process.env.REACT_APP_API_URL;
 
 const Comments = () => {
   const [comments, setComments] = useState([]);
@@ -8,8 +9,9 @@ const Comments = () => {
     const fetchCommentsAndThreads = async () => {
       try {
         // Adjust the URL to point to your backend endpoint for fetching comments
-        const response = await axios.get('/api/comments');
+        const response = await axios.get(`${API}api/comments`);
         setComments(response.data);
+        console.log(response)
       } catch (error) {
         console.error('Error fetching comments:', error);
       }
@@ -21,14 +23,14 @@ const Comments = () => {
   return (
     <div>
       <h3>Let's Display Progrades Below!</h3>
-      {comments.map((comment, index) => (
+      {/* {comments.map((comment, index) => (
         <div key={index}>
           <h2>{comment.text}</h2>
           {comment.threads.map((thread, tIndex) => (
             <p key={tIndex}>{thread.title}</p>
           ))}
         </div>
-      ))}
+      ))} */}
     </div>
   );
 };
