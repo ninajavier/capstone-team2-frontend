a5fe5371cf714e6ce93e91c1d0e16d879513de54
laura-williams-1 <laurawilliams@pursuit.org>
2023-09-27 19:54:26 -0400

    api call is working along with route to mnrAlerts

--- src/Components/mnrAlerts.js
+++ src/Components/mnrAlerts.js
@@ -1,9 +1,28 @@
-const mnrAlerts = () => {
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const MnrAlerts = () => {
+  const [metroNorthRailroadAlerts, setMetroNorthRailroadAlerts] = useState({});
+
+  useEffect(() => {
+    const apiUrl = `https://prograde.onrender.com/all-service-alerts`;
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setMetroNorthRailroadAlerts(response.data);
+        console.log(metroNorthRailroadAlerts);
+      })
+      .catch((error) => {
+        console.error("Error fetching service alerts:", error);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>mnr alerts</h1>
+      <h1>Mnr Alerts</h1>
     </div>
   );
 };
 
-export default mnrAlerts;
+export default MnrAlerts;
