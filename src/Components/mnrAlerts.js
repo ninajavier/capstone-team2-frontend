3027d2c617bd8a9febbe31edbcf0f5141ec73eaa
laura-williams-1 <laurawilliams@pursuit.org>
2023-10-01 07:32:10 -0400

    time function complete

--- src/Components/mnrAlerts.js
+++ src/Components/mnrAlerts.js
@@ -29,6 +29,48 @@ function formatAlertPeriod(alert) {
     "December",
   ];
   // months for time date display
+
+  const formatTime = (date) => {
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    const ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    return `${hours}:${(minutes < 10 ? "0" : "") + minutes} ${ampm}`;
+  };
+
+  if (
+    startDate.getDate() === endDate.getDate() &&
+    startDate.getMonth() === endDate.getMonth()
+  ) {
+    // same end /start date
+    const formattedStartDate = `${
+      monthNames[startDate.getMonth()]
+    } ${startDate.getDate()}, ${startDate.getFullYear()} ${formatTime(
+      startDate
+    )}`;
+    const formattedEndDate =
+      endDate.getTime() === Number.MAX_SAFE_INTEGER
+        ? "until further notice"
+        : formatTime(endDate);
+    return `${formattedStartDate} - ${formattedEndDate} EST`;
+  } else if (endDate.getTime() === Number.MAX_SAFE_INTEGER) {
+    // no end date
+    return `Active as of ${
+      monthNames[startDate.getMonth()]
+    } ${startDate.getDate()}, ${startDate.getFullYear()} ${formatTime(
+      startDate
+    )} EST until further notice`;
+  } else {
+    return `${
+      monthNames[startDate.getMonth()]
+    } ${startDate.getDate()}, ${startDate.getFullYear()} ${formatTime(
+      startDate
+    )} EST - ${
+      monthNames[endDate.getMonth()]
+    } ${endDate.getDate()}, ${endDate.getFullYear()} ${formatTime(
+      endDate
+    )} EST`;
+  }
 }
 
 const MnrAlerts = () => {
