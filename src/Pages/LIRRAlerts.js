98fd2bf83697ec40cb1c1497f87ba1a5bd7cbc1b
laura-williams-1 <laurawilliams@pursuit.org>
2023-10-01 08:23:06 -0400

    sucessful api res

--- src/Pages/LIRRAlerts.js
+++ src/Pages/LIRRAlerts.js
@@ -1,4 +1,17 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 const LIRRAlerts = () => {
+  const [lirrAlerts, setLirrAlerts] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("https://prograde.onrender.com/all-service-alerts")
+      .then((res) => {
+        setLirrAlerts(res.data);
+        console.log(lirrAlerts);
+      })
+      .catch((error) => console.error("Error Fetching LIRR Alerts", error));
+  }, []);
   return (
     <div>
       <h1>LIRR</h1>
